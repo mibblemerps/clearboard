3d2d8232027d0d2289299b7f70ca0c9f61d8712b
Mitchfizz05 <me@mitchfizz05.net>
2015-10-08 23:45:05 +1030

    Add placeholder welcome page

---
+++ resources/views/clearboard/welcome.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Clearboard</title>
+        
+        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+        
+        <style type="text/css">
+            h1 {
+                color: #333333;
+                font-family: "lato", arial, sans-serif;
+                font-size: 24px;
+                
+                font-style: italic;
+            }
+            
+            #wrapper {
+                margin: 0 auto;
+                margin-top: 76px;
+                width: 550px;
+                
+                text-align: center;
+            }
+        </style>
+    </head>
+    <body>
+        <div id="wrapper">
+            <img src="{{ asset('clearboard/assets/header-inverted.png') }}"><br>
+            <h1>Hello.</h1>
+        </div>
+    </body>
+</html>
