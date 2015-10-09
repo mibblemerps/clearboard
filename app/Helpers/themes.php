9a66237b86786a005ed1e66c40ae649bf6f48a1d
Mitchfizz05 <me@mitchfizz05.net>
2015-10-09 14:52:48 +1030

    Add helper function to get theme assets

--- app/Helpers/themes.php
+++ app/Helpers/themes.php
@@ -24,6 +24,7 @@ function joinPath() { // Credit - http://stackoverflow.com/a/1091219/3764348
 /**
  * Returns the base path of the theme's personal asset directory.
  * Similar to Laravel's *_path() functions, a path argument can be supplied to produce a full path.
+ * WARNING! Does not return URL!
  * @param string $path Optional. Path to resource.
  * @param string|null $theme Name of theme. If left null, will use current theme.
  * @return string
@@ -32,4 +33,18 @@ function theme_path($path = '', $theme = null) {
     if ($theme == null) { $theme = config('clearboard.theme'); }
 
     return app_path() . "/assets/$theme/" . ($path ? DIRECTORY_SEPARATOR.$path : $path);
+}
+
+/**
+ * Identical behaviour to Laravel's inbuilt asset() helper function, expect returns assets from a theme's personal
+ * asset directory.
+ * @param $path
+ * @param null $secure
+ * @param null $theme
+ * @return string
+ */
+function theme_asset($path, $secure = null, $theme = null) {
+    if ($theme == null) { $theme = config('clearboard.theme'); }
+
+    return asset("assets/$theme/" . $path, $secure);
 }
