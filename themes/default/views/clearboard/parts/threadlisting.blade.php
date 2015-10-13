f642f72b42015caffb7ba55c6d414c478bed3e2a
Mitchfizz05 <me@mitchfizz05.net>
2015-10-13 20:03:29 +1030

    Add basic view forum page

---
+++ themes/default/views/clearboard/parts/threadlisting.blade.php
@@ -0,0 +1,12 @@
+<div class="listing-entry">
+    <img src="{{ theme_asset('img/unread.png') }}" alt="" class="listing-forum-hasread">
+    <div>
+        <div class="listing-entry-name">
+            <a href="{{ url('thread/' . $thread->id . '-' . urlencode(str_replace(' ', '_', $thread->name))) }}">{{ $thread->name }}</a>
+        </div>
+        <br>
+        <div class="listing-entry-desc">
+
+        </div>
+    </div>
+</div>
