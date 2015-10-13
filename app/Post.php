664158965859bf6200bbbdbfcb09fa224c63902e
Mitchfizz05 <me@mitchfizz05.net>
2015-10-13 19:37:36 +1030

    Add reverse relationship to parent thread inside post model

--- app/Post.php
+++ app/Post.php
@@ -7,4 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     protected $table = 'posts';
+
+    /**
+     * Get the thread containing the post
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function thread()
+    {
+        return $this->belongsTo('App\Thread');
+    }
 }
