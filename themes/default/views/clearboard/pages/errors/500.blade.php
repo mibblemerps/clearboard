f6e1e28c3cf368aa6335330d5691dfa9be0e0eda
Mitchfizz05 <me@mitchfizz05.net>
2015-10-17 18:44:03 +1030

    Add 500 Internal Server Error page

---
+++ themes/default/views/clearboard/pages/errors/500.blade.php
@@ -0,0 +1,14 @@
+@extends('clearboard.main.template')
+
+@section('title', '500 Internal Server Error')
+
+@section('content')
+    <h1>500 Internal Server Error</h1>
+    <p>This one's not your fault...</p>
+    <p>If the problem persists, please contact the forum administrator.</p>
+    <ul>
+        <li><a href="{{ url() }}">Goto the homepage</a></li>
+    </ul>
+@endsection
+
+
