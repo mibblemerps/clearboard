a32cf8f324ba777fbe370960de29c8e20e411a84
Mitchfizz05 <me@mitchfizz05.net>
2015-10-17 18:05:53 +1030

    Add user friendly 404 page

--- themes/default/views/clearboard/pages/errors/404.blade.php
+++ themes/default/views/clearboard/pages/errors/404.blade.php
@@ -1,1 +1,13 @@
-Not found. :(
+@extends('clearboard.main.template')
+
+@section('title', '404 Not Found')
+
+@section('content')
+    <h1>404 Not Found</h1>
+    <p>The page you requested doesn't exist.</p>
+    <ul>
+        <li><a href="{{ url() }}">Goto the homepage</a></li>
+    </ul>
+@endsection
+
+
