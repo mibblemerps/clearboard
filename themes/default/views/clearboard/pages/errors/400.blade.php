3cc8451c8fb460f5b23be7b9de14a3268142804c
Mitchfizz05 <me@mitchfizz05.net>
2015-10-17 18:42:22 +1030

    Add 400 Bad Request error page

---
+++ themes/default/views/clearboard/pages/errors/400.blade.php
@@ -0,0 +1,12 @@
+@extends('clearboard.main.template')
+
+@section('title', '400 Bad Request')
+
+@section('content')
+    <h1>400 Bad Request</h1>
+    <ul>
+        <li><a href="{{ url() }}">Goto the homepage</a></li>
+    </ul>
+@endsection
+
+
