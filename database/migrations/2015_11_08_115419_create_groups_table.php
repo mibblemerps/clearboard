89820bc48d25cd7f8a59b81e4c089e32592b97e7
Mitchfizz05 <me@mitchfizz05.net>
2015-11-09 16:14:41 +1030

    Add Group model and database migration

---
+++ database/migrations/2015_11_08_115419_create_groups_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateGroupsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('groups', function (Blueprint $table) {
+            $table->increments('id'); // Group ID
+            $table->string('name'); // Group name (human-readable)
+            $table->integer('inherits'); // Inherited group
+            $table->text('perms'); // Permission nodes
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('groups');
+    }
+}
