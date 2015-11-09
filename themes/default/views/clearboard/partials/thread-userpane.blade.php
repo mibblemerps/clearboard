3c839d23e4bee8d97486cdb26d27bfff9403d826
Mitchfizz05 <me@mitchfizz05.net>
2015-11-09 16:22:40 +1030

    Remove admin badge from displaying on every post

--- themes/default/views/clearboard/partials/thread-userpane.blade.php
+++ themes/default/views/clearboard/partials/thread-userpane.blade.php
@@ -1,3 +1,2 @@
 <img src="{{ $post->poster()->first()->avatarUrl() }}" alt="" class="post-avatar">
 <div class="post-username">{{ $post->poster()->first()->name }}</div>
-<div class="post-badge">Admin</div>
