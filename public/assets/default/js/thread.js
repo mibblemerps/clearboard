779fdd103374a6edb27d555e23aaace7a6f7caa3
Mitchfizz05 <me@mitchfizz05.net>
2015-11-14 18:41:32 +1030

    Disable SimpleMDE autosaving, as it retains it would retain it's content after posting

--- public/assets/default/js/thread.js
+++ public/assets/default/js/thread.js
@@ -2,11 +2,12 @@
 $(document).ready(function(){
     var simplemde = new SimpleMDE({
         element: $("#postreply")[0],
-        autosave: {
+        // Autosave disabled temporarily because it keeps it's content even after submitting (Clearboard will use a custom AJAX submit function)
+        /*autosave: {
             enabled: true,
             delay: 2000,
             unique_id: "thread-" + window.thread_id // save drafts based on thread id
-        },
+        },*/
         spellChecker: false
     });
 });
