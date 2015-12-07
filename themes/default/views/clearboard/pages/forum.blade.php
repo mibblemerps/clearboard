59fc2c7584809309a86f44ad266b9a090597f6fc
Mitchfizz05 <me@mitchfizz05.net>
2015-12-07 11:22:19 +1030

    Add new thread button

--- themes/default/views/clearboard/pages/forum.blade.php
+++ themes/default/views/clearboard/pages/forum.blade.php
@@ -9,7 +9,10 @@
 @section('content')
     <h1>Viewing Forum &dash; {{ $forum->name }}</h1><br>
     <div id="listing">
-        <div class="listing-category">{{ $forum->name }}</div>
+        <div class="listing-category">
+            {{ $forum->name }}
+            <a href="{{ url('/newthread/' . $forum->id) }}"><span class="button button-green newthread-button">New Thread</span></a>
+        </div>
 
         @foreach($forum->threads as $thread)
             @include('clearboard.partials.threadlisting', ['thread' => $thread])
