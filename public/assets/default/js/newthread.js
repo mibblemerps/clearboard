6944295a9700e1551e6a8876c76dc431d822faf3
Mitchfizz05 <me@mitchfizz05.net>
2015-12-07 11:30:37 +1030

    Add new thread page (has no route yet)

---
+++ public/assets/default/js/newthread.js
@@ -0,0 +1,24 @@
+
+var posteditor;
+
+$(document).ready(function(){
+    posteditor = new SimpleMDE({
+        element: $("#newthread-editor")[0],
+        // Autosave disabled temporarily because it keeps it's content even after submitting (Clearboard will use a custom AJAX submit function)
+        /*autosave: {
+         enabled: true,
+         delay: 2000,
+         unique_id: "thread-" + window.thread_id // save drafts based on thread id
+         },*/
+        spellChecker: false,
+
+        // Use remote server to process markdown preview, for best accuracy.
+        previewRender: function(content, preview){
+            processMarkdown(content, false, function(data){
+                preview.innerHTML = data;
+            });
+
+            return '<span style="color:#575757;font-weight:bold;">Loading, please wait...</span>';
+        }
+    });
+});
