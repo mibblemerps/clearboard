dbe288c72300457b2ad3154ed745a610a2d56d26
Mitchfizz05 <me@mitchfizz05.net>
2015-12-07 14:02:44 +1030

    Implemented user friendly url generator on thread listing

--- themes/default/views/clearboard/partials/threadlisting.blade.php
+++ themes/default/views/clearboard/partials/threadlisting.blade.php
@@ -2,7 +2,7 @@
     <img src="{{ theme_asset('img/unread.png') }}" alt="" class="listing-forum-hasread">
     <div>
         <div class="listing-entry-name">
-            <a href="{{ url('thread/' . $thread->id . '-' . urlencode(str_replace(' ', '_', $thread->name))) }}">{{ $thread->name }}</a>
+            <a href="{{ $thread->getUserFriendlyURL() }}">{{ $thread->name }}</a>
         </div>
         <br>
         <div class="listing-entry-desc">
