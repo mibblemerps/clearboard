3a4b6941a407a1ec55ea4069d9e10996b9e89e42
Mitchfizz05 <me@mitchfizz05.net>
2015-12-10 19:46:01 +1030

    Add registration error mesage support

--- public/assets/default/js/register.js
+++ public/assets/default/js/register.js
@@ -1,3 +1,37 @@
+
 /**
- * Created by me on 8/12/2015.
+ * Display registration errors to the user.
+ * Pass a blank array of errors to hide the error message.
+ * @param errors Array of error identifiers
  */
+function displayRegisterErrors(errors) {
+    if (errors.length == 0) {
+        // No errors passed, hide error message
+        $(".register-error").slideUp(200);
+        return;
+    }
+
+    // Display each neccesary message for each error.
+    $(".register-error li").each(function() {
+        if ( $.inArray($(this).attr("data-error"), errors) != -1 ) {
+            $(this).show();
+        } else {
+            $(this).hide();
+        }
+    });
+
+    // Now show the actual error message
+    $(".register-error").slideDown(200);
+}
+
+function register(email, username, password) {
+    // Send registration request to server
+    $.post("/ajax/register", {
+        _token: window.csrf_token,
+        email: email,
+        username: username,
+        password: password
+    });
+
+
+}
