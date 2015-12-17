cd142203855628098af401cebd07994ea99df7c7
Mitchfizz05 <me@mitchfizz05.net>
2015-12-17 11:10:00 +1030

    Add reverse-relationship with the User Eloquent and the Group Eloquent.

--- app/User.php
+++ app/User.php
@@ -31,6 +31,11 @@ class User extends Model implements AuthenticatableContract,
      */
     protected $hidden = ['password', 'remember_token'];
 
+    public function group()
+    {
+        return $this->belongsTo('App\Group');
+    }
+
     /**
      * Get link to this users avatar
      * @param int $size Size in pixels. All avatars are square.
