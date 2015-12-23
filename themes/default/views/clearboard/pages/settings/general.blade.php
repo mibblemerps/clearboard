16bfb6557e706f380e94b1e34ec95258d286bd6d
Mitchfizz05 <me@mitchfizz05.net>
2015-12-24 05:39:42 +1030

    Add note regarding Gravatar to the settings page

--- themes/default/views/clearboard/pages/settings/general.blade.php
+++ themes/default/views/clearboard/pages/settings/general.blade.php
@@ -1,2 +1,7 @@
-<h1>General Settings</h1>
-<em>Work in progress...</em>
+<h2>Profile Picture</h2>
+<div class="vertically-aligned">
+    <img src="{{ Auth::user()->getAvatarUrl(64) }}" alt="{{ Auth::user()->name }}">
+    <span>
+        Profile pictures are currently handled through <a href="https://gravatar.com/">Gravatar</a>.
+    </span>
+</div>
