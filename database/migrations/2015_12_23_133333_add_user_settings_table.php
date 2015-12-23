a0cf3e95270fb8c3c906d7983f32cd91120e17d0
Mitchfizz05 <me@mitchfizz05.net>
2015-12-24 01:44:06 +1030

    Add database migration for user settings

---
+++ database/migrations/2015_12_23_133333_add_user_settings_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddUserSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_settings', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id');
+            $table->string('key');
+            $table->string('value');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_settings');
+    }
+}
