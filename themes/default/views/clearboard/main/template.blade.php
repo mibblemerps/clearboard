e7f229d225002a84818b3a430d46be200d2880be
Mitchfizz05 <me@mitchfizz05.net>
2015-12-27 16:47:37 +1030

    Changed post body font to Source Sans Pro Light

--- themes/default/views/clearboard/main/template.blade.php
+++ themes/default/views/clearboard/main/template.blade.php
@@ -71,7 +71,7 @@
         </footer>
 
         <!-- Low priority assets -->
-        <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700|Lato|Merriweather'">
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Source+Sans+Pro:400,200,300|Open+Sans:400,700|Lato">
         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" href="{{ theme_asset('libs/load-awesome/css/ball-scale-pulse.min.css') }}">
     </body>
