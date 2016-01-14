841b397fdbe344cc76b2c51cae8504531eb80934
Mitchfizz05 <me@mitchfizz05.net>
2016-01-14 00:25:50 +0000

    Badges are now displayed next to posts.

--- themes/default/views/clearboard/thread/partials/userinfo.blade.php
+++ themes/default/views/clearboard/thread/partials/userinfo.blade.php
@@ -1,2 +1,5 @@
 <img src="{{ $post->poster()->first()->getAvatarUrl() }}" alt="" class="post-avatar">
 <div class="post-username">{!! $post->poster()->first()->getStyledUsername() !!}</div>
+@if($post->poster()->first()->hasBadge())
+    <img class="post-badge" src="{!! $post->poster()->first()->getBadge() !!}" alt="{{ $post->poster()->first()->group->name }}">
+@endif
