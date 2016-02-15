8f0815161b74476768f3031605b36b552cc954c6
Mitchfizz05 <me@mitchfizz05.net>
2016-02-15 18:39:05 +1030

    Add execution time to footer debug information.

--- themes/default/views/clearboard/common/footer.blade.php
+++ themes/default/views/clearboard/common/footer.blade.php
@@ -16,6 +16,10 @@
                 $githead = exec('git rev-parse --short=7 HEAD');
                 echo "<span><strong>Git HEAD: </strong> <em>$githead</em> </span>";
             }
+
+            // Script execution time
+            $executiontime = round(microtime(true) - SCRIPT_START, 4);
+            echo "<span><strong>Generated in: </strong> <em>{$executiontime}s</em> </span>";
         ?>
     </p>
 @endif
