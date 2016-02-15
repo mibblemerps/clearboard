630c69b66d65161b84bad4e91ce31defc0d57da8
Mitchfizz05 <me@mitchfizz05.net>
2016-02-15 17:57:32 +1030

    Now overriding Laravel's @can, @cannot, @endcan directives and replacing them with Clearboard's permission node alternatives.

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -15,16 +15,27 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // Blade directive to check if user has permission node.
         Blade::directive('hasperm', function ($expression) {
             return "<?php if (Auth::user()->can({$expression})): ?>";
         });
 
+        // Blade directive to see if user is missing permission node.
         Blade::directive('missingperm', function ($expression) {
             return "<?php if (!Auth::user()->can({$expression})): ?>";
         });
 
+        // Blade directive to end permission node check block.
         Blade::directive('endperm', function ($expression) {
             return '<?php endif; ?>';
+        });
+
+        // Override @can, @cannot and @endcan to permission node checks.
+        Blade::extend(function ($view) {
+            $view = str_replace('@can', '@hasperm', $view);
+            $view = str_replace('@cannot', '@missingperm', $view);
+            $view = str_replace('@endcan', '@endperm', $view);
+            return $view;
         });
     }
 
