87f5e8fd457a1f005faf616524b95064b5deebcb
Mitchfizz05 <me@mitchfizz05.net>
2016-02-17 21:54:43 +1030

    Replaced lots of classes with IDs where appropriate.

--- themes/default/views/clearboard/profile/viewprofile.blade.php
+++ themes/default/views/clearboard/profile/viewprofile.blade.php
@@ -7,16 +7,16 @@
 @endsection
 
 @section('content')
-    <div class="profile-header">
-        <div class="profile-buttons">
+    <div id="profile-header">
+        <div id="profile-buttons">
             <span class="button">Edit Profile</span>
         </div>
 
-        <img src="{{ $user->getAvatarUrl(128) }}" alt="{{ $user->name }}" class="profile-image">
-        <div class="profile-header-inner">
+        <img src="{{ $user->getAvatarUrl(128) }}" alt="{{ $user->name }}" id="profile-image">
+        <div id="profile-header-inner">
             <div id="profile-username">{{ $user->name }}</div><br>
-            <div class="profile-group">{!! $user->group->styleUsername($user->group->name) !!}</div>
-            <div class="profile-infos">
+            <div id="profile-group">{!! $user->group->styleUsername($user->group->name) !!}</div>
+            <div id="profile-info">
                 <div class="profile-info">
                     <i class="fa fa-clock-o"></i>
                     Last active {{ format_time($user->last_active) }}
