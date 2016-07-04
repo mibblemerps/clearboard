3c23da2743dfd021d6665731508d9b40977f834e
Mitchfizz05 <me@mitchfizz05.net>
2016-07-04 10:39:03 +0930

    Add migration adding activation_code column to users table.

---
+++ database/migrations/2016_07_04_010644_add_activtion_code_column_to_users_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddActivtionCodeColumnToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('activation_code', 64)->after('remember_token')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('activation_code');
+        });
+    }
+}
